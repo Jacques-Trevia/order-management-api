3e8ae523beee9ac6a2b5f160628109aaddd144e9
Jacques-Trevia <jacquesceara@gmail.com>
2026-08-03 22:31:21 -0300

    chore: initial project setup with Spring Initializr
    - Generate base project structure with Spring Boot 4.1.0
    - Add core dependencies: Web, JPA, PostgreSQL, Validation, Lombok

---
+++ src/main/java/ecommerce_order_api/EcommerceOrderApiApplication.java
@@ -0,0 +1,13 @@
+package ecommerce_order_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceOrderApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceOrderApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ecommerce_order_api/EcommerceOrderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ecommerce_order_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceOrderApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
